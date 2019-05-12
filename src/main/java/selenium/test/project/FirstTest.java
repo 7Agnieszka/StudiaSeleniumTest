a9d55374d402ccea917d4af6c6c9ea0489767599
7Agnieszka <aga.o93@gmail.com>
2019-05-12 10:36:53 +0200

    first

---
+++ src/main/java/selenium/test/project/FirstTest.java
@@ -0,0 +1,26 @@
+package selenium.test.project;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void test(){
+
+        Assert.assertEquals(4,4);
+    }
+
+
+    @Test
+    public void test2(){
+
+        Assert.assertEquals(3,4);
+    }
+
+    @Test
+    public void test3(){
+
+        Assert.assertEquals(3,4);
+    }
+}
