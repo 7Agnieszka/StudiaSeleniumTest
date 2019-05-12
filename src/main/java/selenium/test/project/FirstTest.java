478a04add535c8544f33436c53f608f1cbfe8dd9
aga <aga.o93@gmail.com>
2019-05-12 11:16:16 +0200

    Browser open

--- src/main/java/selenium/test/project/FirstTest.java
+++ src/main/java/selenium/test/project/FirstTest.java
@@ -10,7 +10,10 @@ public class FirstTest {
     @Test
     public void test() {
 
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\zp114278\\Desktop\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.pl/");
+
 
     }
 
