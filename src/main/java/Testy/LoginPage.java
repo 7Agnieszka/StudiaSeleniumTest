aed80eda5bcf621cca167a4ac5a0c31f5a0d4e73
aga <aga.o93@gmail.com>
2019-05-25 09:42:25 +0200

    First POP

--- src/main/java/Testy/LoginPage.java
+++ src/main/java/Testy/LoginPage.java
@@ -2,23 +2,46 @@ package Testy;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
     WebDriver driver;
 
+    @FindBy(id = "email")
+    WebElement emailField;
+
+    @FindBy(id = "password")
+    WebElement loginField;
+
+    @FindBy(id = "login")
+    WebElement loginButon;
+
+
     public LoginPage(WebDriver driver){
         this.driver = driver;
+
+        PageFactory.initElements(this.driver,this);
+
     }
 
+
     public LoginPage fillLoginForm(String username, String password) {
-        driver.findElement(By.id("id_username")).sendKeys(username);
-        driver.findElement(By.id("id_password")).sendKeys(password);
+       keysSendToClearField(emailField, username);
+       keysSendToClearField(loginField, password);
         return this;
     }
 
+    public void keysSendToClearField(WebElement e, String text){
+        e.clear();
+        e.sendKeys(text);
+    }
+
     public DashboardPage submitLoginForm() {
-        driver.findElement(By.cssSelector("#login > div:nth-child(5) > div > button")).click();
+        //driver.findElement(By.cssSelector("#login > div:nth-child(5) > div > button")).click();
+        loginButon.click();
         return new DashboardPage(driver);
     }
 }
